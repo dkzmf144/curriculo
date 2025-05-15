14b33f854c6a54a7dce1b8bb455b0d03e6e7be28
dkzmf144 <dkzmf144@gmail.com>
2025-05-15 11:38:47 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-function showSection(id) {
-  document.querySelectorAll("section").forEach((section) => {
-    section.classList.remove("active");
-  });
-  document.getElementById(id).classList.add("active");
-}
-
 function enviarMensagem() {
-  alert(
-    "Função de envio de mensagem acionada! Aqui poderia ir para o WhatsApp ou Gmail real."
-  );
+  const numero = "5511943679193";
+  const mensagem = "Olá Isaque, vi seu currículo online!";
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+  window.open(url, "_blank");
 }
